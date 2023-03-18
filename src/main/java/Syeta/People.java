ff6f149640985b883a320f09de9b3e5b2214fdd0
denis <kuchukovdenis2003@mail.ru>
2023-03-18 14:35:02 +0500

    6 лаба

---
+++ src/main/java/Syeta/People.java
@@ -0,0 +1,49 @@
+package Syeta;
+
+public class People {
+    private String login;
+    private String password;
+    private String email;
+    private int id;
+
+    public People(String login, String password, String email) {
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+    public People(int id, String login, String password, String email) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    public People(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
